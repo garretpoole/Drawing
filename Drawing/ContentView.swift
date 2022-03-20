e78205210786f61f782be67b3765c4c8b335c81f
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-20 10:30:44 -0700

    Triangle with created Shape struct

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,18 +7,36 @@
 
 import SwiftUI
 
+struct Triangle: Shape{
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        //creates triangle freely using call to Rect
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        return path
+    }
+}
+
+struct Arc: Shape{
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            //prevents a break at the top
-            //path.closeSubpath()
-        }
-        //different way to prevent breaks between lines
-        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        //main difference with Shapes is reusability
+        Triangle()
+            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
     }
 }
 
