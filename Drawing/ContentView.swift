fff7291ac717f5ebbea80e92132f6ac1c3fec485
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-22 16:14:41 -0700

    animating Shapes with animatableData and withAnimation

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,44 +7,38 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape{
+    var insetAmount: Double
+    //for withAnimation, as animation progresses it constantly sets the animatableData property of the shape to its latest value and is up to us to decide what that means
+    var animatableData: Double{
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
-    @State private var amount = 0.0
+    @State private var insetAmount = 50.0
 
     var body: some View{
-        VStack{
-            //image with blur and saturation based on slider
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .saturation(amount)
-                .blur(radius: (1-amount)*20)
-            
-            //Three Circle
-//            ZStack{
-//                Circle()
-//                    .fill(Color(red: 1, green: 0, blue: 0))
-//                    .frame(width:200*amount)
-//                    .offset(x: -50, y: -80)
-//                    .blendMode(.screen)
-//                Circle()
-//                    .fill(Color(red: 0, green: 1, blue: 0))
-//                    .frame(width:200*amount)
-//                    .offset(x: 50, y: -80)
-//                    .blendMode(.screen)
-//                Circle()
-//                    .fill(Color(red: 0, green: 0, blue: 1))
-//                    .frame(width:200*amount)
-//                    .blendMode(.screen)
-//            }
-//            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 200)
+            .onTapGesture {
+                withAnimation{
+                    insetAmount = Double.random(in: 10...90)
+                }
+            }
     }
 }
 
