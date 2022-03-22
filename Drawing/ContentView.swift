3eef38fc8dde3214c753922256bb4d6ac496f375
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-22 15:30:49 -0700

    Borders using ImagePaint and Images

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,44 +7,15 @@
 
 import SwiftUI
 
-struct Flower: Shape{
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path{
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi*2, by: Double.pi/8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width/2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        return path
-    }
-}
 
 struct ContentView: View {
     @State private var petalOffset = -20.0
     @State private var petalWidth = 100.0
     
     var body: some View{
-        VStack{
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: -40...40)
-                .padding(.horizontal)
-        }
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 0.25, height: 0.5), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
 
