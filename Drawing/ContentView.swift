19c34a6bcb617ca8aedc18fbcd8e2a3f5ee61a53
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-22 18:28:50 -0700

    struct Arrow with animatable thickness

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,91 +7,44 @@
 
 import SwiftUI
 
-struct Spirograph: Shape{
-    let innerRadius: Int
-    let outerRadius: Int
-    let distance: Int
-    let amount: Double
-    
-    func gcd(_ a: Int, _ b: Int) -> Int{
-        var a = a
-        var b = b
-        
-        while b != 0{
-            let temp = b
-            b = a % b
-            a = temp
-        }
-        return a
+struct Arrow: Shape{
+    var thickness: Double
+    var animatableData: Double{
+        get { thickness }
+        set { thickness = newValue }
     }
     
     func path(in rect: CGRect) -> Path {
-        let divisor = gcd(innerRadius, outerRadius)
-        
-        let outerRadius = Double(self.outerRadius)
-        let innerRadius = Double(self.innerRadius)
-        let distance = Double(self.distance)
-        
-        let difference = innerRadius - outerRadius
-        let endPoint = ceil(2*Double.pi * outerRadius / Double(divisor)) * amount
-        
         var path = Path()
-        for theta in stride(from: 0, through: endPoint, by: 0.01) {
-            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
-            
-            x += rect.width / 2
-            y += rect.height / 2
-            
-            if theta == 0{
-                path.move(to: CGPoint(x: x, y: y))
-            } else {
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
+        path.move(to: CGPoint(x: rect.midX+thickness, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX-thickness, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX-thickness, y: rect.minY+2*thickness))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY+thickness))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY-thickness))
+        path.addLine(to: CGPoint(x: rect.midX-thickness, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX+thickness, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY-thickness))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY+thickness))
+        path.addLine(to: CGPoint(x: rect.midX+thickness, y: rect.minY+2*thickness))
+        path.addLine(to: CGPoint(x: rect.midX+thickness, y: rect.maxY))
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount = 1.0
-    @State private var hue = 0.6
-    
+    @State private var thickness = 20.0
     var body: some View{
         VStack(spacing: 0){
             Spacer()
-            
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
-                .frame(width: 300, height: 300)
-            
+            Arrow(thickness: thickness)
+                .onTapGesture {
+                    withAnimation{
+                        thickness+=10
+                    }
+                }
+                .frame(width: 250, height: 350)
             Spacer()
-            
-            Group{
-                Text("Inner Radi=us: \(Int(innerRadius))")
-                Slider(value: $innerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Outer Radius: \(Int(outerRadius))")
-                Slider(value: $outerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Distance: \(Int(distance))")
-                Slider(value: $distance, in: 1...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
-                Slider(value: $amount)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Color")
-                Slider(value: $hue)
-                    .padding(.horizontal)
-                
-            }
+    
         }
        
     }
